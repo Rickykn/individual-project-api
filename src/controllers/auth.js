6747e774d2113b9227a81a2f248480ef62faf907
Ricky Kurniawan <neakre@Rickys-MacBook-Pro.local>
2022-04-05 14:12:46 +0700

    create login user api

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,7 @@ const { Op } = require("sequelize");
 const { User } = require("../lib/sequelize");
 const bcrypt = require("bcrypt");
 const fs = require("fs");
+const { generateToken } = require("../lib/jwt");
 
 const authControllers = {
   registerUser: async (req, res) => {
@@ -29,6 +30,51 @@ const authControllers = {
 
       return res.status(201).json({
         message: "Registered user",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  },
+  loginUser: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      const findUser = await User.findOne({
+        where: {
+          username,
+        },
+      });
+
+      if (!findUser) {
+        return res.status(400).json({
+          message: "Wrong username or password",
+        });
+      }
+
+      const isPasswordCorrect = bcrypt.compareSync(password, findUser.password);
+
+      if (!isPasswordCorrect) {
+        return res.status(400).json({
+          message: "Wrong username or password",
+        });
+      }
+
+      delete findUser.dataValues.password;
+
+      const token = generateToken({
+        id: findUser.id,
+        role: findUser.role,
+      });
+
+      return res.status(200).json({
+        message: "Logged in user",
+        result: {
+          user: findUser,
+          token,
+        },
       });
     } catch (err) {
       console.log(err);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const authControllers = require("../controllers/auth");
 const fileUploader = require("../lib/uploader");
 
 const router = require("express").Router();
-// router.post("/login", authControllers.loginUser);
+router.post("/login", authControllers.loginUser);
 router.post("/register", authControllers.registerUser);
 
 module.exports = router;
